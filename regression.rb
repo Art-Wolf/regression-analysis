12a0773571d87be8909ce1a4055c9c235a09d721
Art-Wolf <john.c.doyle@gmail.com>
2014-03-26 00:26:32 -0400

    still trying to get r..

--- regression.rb
+++ regression.rb
@@ -8,22 +8,56 @@ y1_data = [7100, 15500, 4400, 4400, 5900, 4600, 8800, 2000, 2750, 2550, 960, 102
 
 x1_count = 0
 x1_mean = 0
+x1_var = 0
+x1_ss = 0
 
 for number in x1_data
   x1_count += 1
-  x1_mean += number  
+  x1_ss += number  
 end
 
-x1_mean = x1_mean/x1_count
-
-puts x1_mean 
-
-x1_var = 0
+x1_mean = x1_ss/x1_count
 
 for number in x1_data
   x1_var += ((number - x1_mean) * (number - x1_mean))
 end
-puts x1_count
-x1_var = x1_var/x1_count
 
-puts Math.sqrt(x1_var)
+x1_var = x1_var/(x1_count - 1)
+x1_std = Math.sqrt(x1_var)
+
+y1_count = 0
+y1_mean = 0
+y1_var = 0
+y1_ss = 0
+
+for number in y1_data
+  y1_count += 1
+  y1_ss += number
+end
+
+y1_mean = y1_ss/y1_count
+
+for number in y1_data
+  y1_var += (number - y1_mean) * (number - y1_mean)
+end
+
+y1_var = y1_var/(y1_count - 1)
+y1_std = Math.sqrt(y1_var)
+
+x1y1_ss = 0
+x1_data.each_with_index do |number, index|
+  puts y1_data[index]
+  x1y1_ss += (number - x1_mean) * (y1_data[index] - y1_mean)
+end
+
+puts x1y1_ss
+puts x1_var
+puts y1_var
+puts x1_var * y1_var
+puts Math.sqrt(x1_var * y1_var)
+puts Math.sqrt(x1y1_ss*x1y1_ss)/Math.sqrt(x1_var * y1_var)
+
+r_correlation_coefficient = x1y1_ss / Math.sqrt(x1_var * y1_var)
+
+puts Math.sqrt(r_correlation_coefficient)
+
